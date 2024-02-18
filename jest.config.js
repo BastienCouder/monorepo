a1b8be45fc9a0b6a05cf7a71ad2ffa6145e4523a
BastienCouder <bastien.couder@gmail.com>
2024-02-18 11:56:00 +0100

    pre-commit

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: "./",
+  dir: './',
 });
 
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  preset: "ts-jest",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
   verbose: true,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.jest.json'
-    }
-  }
+      tsconfig: '<rootDir>/tsconfig.jest.json',
+    },
+  },
 };
 
 module.exports = createJestConfig(config);
