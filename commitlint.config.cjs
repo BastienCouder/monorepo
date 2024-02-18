99d007cd7c6a1703a0f721f9deffb195d2bd80fd
BastienCouder <bastien.couder@gmail.com>
2024-02-18 13:00:03 +0100

    Added Github actions

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,23 +1,22 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-  // extends: ['@commitlint/config-conventional'],
   extends: [],
   rules: {
-    'header-min-length': [2, 'always', 20],
-    'header-case-start-capital': [2, 'always'],
-    'header-end-period': [2, 'always'],
+    'header-min-length': [2, 'always', 10],
+    'plugin-name/header-start-capital': [2, 'always'],
+    'plugin-name/header-end-period': [2, 'always'],
   },
   plugins: [
     {
       rules: {
-        'header-casestart-capital': ({ raw }) => {
+        'header-start-capital': ({ raw }) => {
           return [
             /^[A-Z]/.test(raw),
             'Commit message must start with a capital letter',
           ];
         },
-        'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit message must end with a period'];
+        'header-end-period': ({ raw }) => {
+          return [/\.$/.test(raw), 'Commit message must end with a period'];
         },
       },
     },
