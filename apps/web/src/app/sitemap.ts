dc66078021020b04e75f32e4b2b2feb94a499016
Turbobot <turbobot@vercel.com>
2025-03-20 00:10:55 +0100

    feat(create-turbo): apply official-starter transform

--- apps/web/src/app/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import type { MetadataRoute } from "next";
-
-const URL = `${process.env.NEXT_PUBLIC_APP_URL}`;
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [];
-}
