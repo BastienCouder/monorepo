858019e756c23580058ec8312c6b04d095baf65e
BastienCouder <bastien.couder@gmail.com>
2025-04-11 22:46:04 +0200

    monitoring

---
+++ ansible/command.md
@@ -0,0 +1,11 @@
+// create file
+ansible-vault create vars/secrets.yml
+
+// view file
+ansible-vault view vars/secrets.yml
+
+// encrypt file
+ansible-vault encrypt vars/secrets.yml
+
+// decrypt file
+ansible-vault decrypt vars/secrets.yml
--- apps/web/db/index.ts
+++ apps/web/db/index.ts
@@ -1,4 +1,39 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-
+import { eq } from 'drizzle-orm';
+import { usersTable } from './schema';
+  
 const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      name: 'John Doe',
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!')
+
+//   await db.delete(usersTable).where(eq(usersTable.email, user.email));
+//   console.log('User deleted!')
+}
+
+main();
